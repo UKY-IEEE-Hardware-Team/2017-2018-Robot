bd60830b147c470ddd2a45ffcf6fb4ecbaacef0c
JarrenTay <jarrentayusa@gmail.com>
2018-04-20 12:56:53 -0400

    Add files via upload
    Added get off ramp, go until in front of wedge, turn to wedge if needed, go to wedge, hit it, back up.

--- FinFunc.ino
+++ FinFunc.ino
@@ -1,5 +1,6 @@
 // MASTER FILE
 #include <LiquidCrystal.h>
+#include <Servo.h>
 
 #define FRONT_LEFT  0
 #define LEFT_RIGHT  1
@@ -10,17 +11,17 @@
 #define RIGHT_LEFT  6
 #define FRONT_RIGHT 7 
 
-LiquidCrystal lcd(8, 9, 4, 5, 6, 7);  // Initialize LCD
+LiquidCrystal lcd(8, 9, 0, 1, 2, 3);  // Initialize LCD
 
 const int switchStop =  0;    // Pin for Switch Stop
 const int switchStart = 1;   // Pin for Switch Start
 
-const int irPin = 2;          // Pin for Solar Panel
+const int irPin = A2;          // Pin for Solar Panel
 
 const int trigPin[8] = {22, 24, 26, 28, 30, 32, 34, 36}; // Pins for Ultrasonic Trigger
 const int echoPin[8] = {23, 25, 27, 29, 31, 33, 35, 37}; // Pins for Ultrasoinc Echo
 
-
+Servo wedger;
 int state = 0;
 
 void setup() {
@@ -28,9 +29,18 @@ void setup() {
   pinMode(switchStop, INPUT_PULLUP);  // Initialize Switch Stop
   pinMode(switchStart, INPUT_PULLUP); // Initialize Switch Start
   lcd.begin(16, 2);                   // Initialize LCD
+  wedger.attach(35);
   for (int i = 0; i < 8; i++) {
     pinMode(trigPin[i], OUTPUT);      // Initialize 8 Trig Pins
     pinMode(echoPin[i], INPUT);       // Initialize 8 Echo Pins
+  }
+}
+
+int path = -1;
+void loop() {
+  if (path == -1 || path == -2){
+    path = readSolarPanel();
+    writePathLCD(path);
   }
 }
 
@@ -216,5 +226,191 @@ double readSharpDistance(int pinNumber)
   Serial.print("\t");
   Serial.println(distanceAverage);
   return distanceAverage;
+}
+
+void goDownAndRightOrUpAndRight(int path) {
+  int sensorClose = -1;
+  double sensorDiff = 0;
+  double read0 = 0;
+  double read7 = 0;
+  double read1 = 0;
+  double read2 = 0;
+  // Drive off ramp
+  drive(0);
+  delay(800); // Drive 0.8 seconds before checking sensors
+  drive(10);
+  read0 = readSensorDistance(0);
+  delay(60);
+  read7 = readSensorDistance(7);
+  delay(140);
+  drive(0);
+  while ((read0 < 15) || (read7 < 15) {
+    read0 = readSensorDistance(0);
+    delay(60);
+    read7 = readSensorDistance(7);
+    delay(60);
+  }
+  drive(10);
+  while ((read0 < 12) || (read7 < 12) {
+    drive(0)
+    delay(60);
+    drive(10);
+    read0 = readSensorDistance(0);
+    delay(60);
+    read7 = readSensorDistance(7);
+  }
+  if (bitRead(path, 6)) {
+    // Turn clockwise twice, go left
+    // Clockwise 1
+    drive(8)
+    delay(500);
+    read0 = readSensorDistance(0);
+    delay(60);
+    read7 = readSensorDistance(7);
+    while(abs(read7 - read0) < 2) {
+      delay(60);
+      read0 = readSensorDistance(0);
+      delay(60);
+      read7 = readSensorDistance(7);
+    }
+
+    // Clockwise 2
+    drive(8)
+    delay(500);
+    read1 = readSensorDistance(1);
+    delay(60);
+    read2 = readSensorDistance(2);
+    while(abs(read1 - read2) < 2) {
+      delay(60);
+      read1 = readSensorDistance(1);
+      delay(60);
+      read2 = readSensorDistance(2);
+    }
+
+    // Make sure perpendicular
+    read1 = readSensorDistance(1);
+    delay(60);
+    read2 = readSensorDistance(2);
+    sensorDiff = read1 - read2;
+    while(abs(sensorDiff) > 1) {
+      if(sensorDiff > 1) {
+        // Turn counter clockwise
+        drive(9);
+        delay(60); 
+      } else if (sensorDiff < -1) {
+        // Turn clockwise
+        drive(8);
+        delay(60);
+      }
+      read1 = readSensorDistance(1);
+      read2 = readSensorDistance(2);
+      sensorDiff = read1 - read2;
+    }
+
+    // Go 6 inches away from the wall Left
+    drive(6);
+    while(read1 < 6 || read2 < 6) {
+      read1 = readSensorDistance(1);
+      delay(60);
+      read2 = readSensorDistance(2);
+      delay(60);
+    }
+    drive(10); 
+  } else {
+    // Robot is in the correct orientation after leaving the ramp, just go left
+    // Make sure perpendicular
+    read1 = readSensorDistance(1);
+    delay(60);
+    read2 = readSensorDistance(2);
+    sensorDiff = read1 - read2;
+    while(abs(sensorDiff) > 1) {
+      if(sensorDiff > 1) {
+        // Turn counter clockwise
+        drive(9);
+        delay(60); 
+      } else if (sensorDiff < -1) {
+        // Turn clockwise
+        drive(8);
+        delay(60);
+      }
+      read1 = readSensorDistance(1);
+      read2 = readSensorDistance(2);
+      sensorDiff = read1 - read2;
+    }
+    
+    // Go 6 inches away from the wall Left
+    drive(6);
+    while(read1 < 6 || read2 < 6) {
+      read1 = readSensorDistance(1);
+      delay(60);
+      read2 = readSensorDistance(2);
+      delay(60);
+    }
+    drive(10); 
+  }
+}
+
+void wedgeAndOff() {
+  // This should move wedger to disengaged position (Should already be here)
+  wedger.write(900);
+  // Align to perpendicular using back and front ultras
+  double read0 = 0;
+  double read7 = 0;
+  double read3 = 0;
+  double read4 = 0;
+  bool perpendicular = false;
+  read0 = readSensorDistance(0);
+  read3 = readSensorDistance(3);
+  delay(60);
+  read7 = readSensorDistance(7);
+  read4 = readSensorDistance(4);
+  if(abs(read0 - read1) > 1 && abs(read3 - read4) > 1) {
+    perpendicular = false;
+  } else {
+    perpendicular = true;
+  }
+  while(!perpendicular) {
+    // Turn if the robot is biased
+    if(read0 > read7 && read4 > read3) {
+      // Needs to turn clockwise a bit
+      drive(8);
+      delay(50);
+      drive(10);
+    } else if(read0 < read7 && read4 < read3) {
+      // Needs to turn counterclockwise a bit
+      drive(9);
+      delay(50);
+      drive(10);
+    } else {
+      // The robot reads opposite biases on each side
+      perpendicular = true;
+    }
+
+    // Check perpendicularity
+    read0 = readSensorDistance(0);
+    read3 = readSensorDistance(3);
+    delay(60);
+    read7 = readSensorDistance(7);
+    read4 = readSensorDistance(4);
+    if(abs(read0 - read1) < 1 || abs(read3 - read4) < 1) {
+      perpendicular = true;
+    }
+  }
+
+  // Engage Wedge motor
+  wedger.write(1700);
+
+  // Run into Wedge
+  drive(6);
+  delay(400);
+  drive(10);
+
+  // Back off Wedge
+  drive(2);
+  delay(200);
+  drive(10);
+
+  // Disengage Wedge
+  wedger.write(900);
 }
 
