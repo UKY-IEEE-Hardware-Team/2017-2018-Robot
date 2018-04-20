6c70a3d7aa16f38965f3fc7edb3bdccd56ed5efa
Luke Wormald <luke.wormald17@gmail.com>
2018-04-20 13:37:10 -0400

    added more defs

--- pin_definitions.h
+++ pin_definitions.h
@@ -14,27 +14,27 @@
   #define LCD_P1 10
   #define LCD_P0 11
   #define LCD_EN 13
-  #define LCD_RS 12  //
-  #define STEPPER_P0 14  //
-  #define STEPPER_P1 15  //
-  #define STEPPER_P2 16  //
-  #define STEPPER_P3 17  //
-  // #define  18  //
-  // #define  19  //
-  // #define  20  //
-  // #define  21  //
-  // #define  22  //
-  // #define  23  //
-  // #define  24  //
-  // #define  25  //
-  // #define  26  //
-  // #define  27  //
-  // #define  28  //
-  // #define  29  //
-  // #define  30  //
-  // #define  31  //
-  // #define  32  //
-  // #define  33  //
+  #define LCD_RS 12
+  #define STEPPER_P0 14
+  #define STEPPER_P1 15
+  #define STEPPER_P2 16
+  #define STEPPER_P3 17
+  #define ULTRASONIC_0_TRIG 18
+  #define ULTRASONIC_0_ECHO 19
+  #define ULTRASONIC_1_TRIG 20
+  #define ULTRASONIC_1_ECHO 21
+  #define ULTRASONIC_2_TRIG 22
+  #define ULTRASONIC_2_ECHO 23
+  #define ULTRASONIC_3_TRIG 24
+  #define ULTRASONIC_3_ECHO 25
+  #define ULTRASONIC_4_TRIG 26
+  #define ULTRASONIC_4_ECHO 27
+  #define ULTRASONIC_5_TRIG 28
+  #define ULTRASONIC_5_ECHO 29
+  #define ULTRASONIC_6_TRIG 30
+  #define ULTRASONIC_6_ECHO 31
+  #define ULTRASONIC_7_TRIG 32
+  #define ULTRASONIC_7_ECHO 33
   // #define  34  //
   // #define  35  //
   // #define  36  //
@@ -56,10 +56,10 @@
   // #define  52  //
   // #define  53  //
   #define SOLAR_CELL A0
-  // #define  A1  //
-  // #define  A2  //
-  // #define  A3  //
-  // #define  A4  //
+  #define SHARP_0 A1
+  #define SHARP_1 A2
+  #define SHARP_2 A3
+  #define SHARP_3 A4
   // #define  A5  //
   // #define  A6  //
   // #define  A7  //
@@ -71,5 +71,8 @@
   // #define  A13 //
   // #define  A14 //
   // #define  A15 //
+
+  const int trigPin[8] = {18, 20, 22, 24, 26, 28, 30, 32};  // Pins for Ultrasonic Trigger
+  const int echoPin[8] = {19. 21, 23, 25, 27, 29, 31, 33};  // Pins for Ultrasoinc Echo
 
 #endif
